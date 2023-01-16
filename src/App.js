3b8c7f82cb7d1e3e7832cb80123d0fce370e96ed
Olivio Gomes Vieira Filho <ogvieira@globo.com>
2023-01-16 16:07:21 -0300

    Clima e tempo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Previsão do tempo</h1>
+        <input type={'text'} width={'250px'}        />
       </header>
     </div>
   );
